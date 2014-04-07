d240f6cf021bcd2681860ebde5e91690c471549e
yangml <yangmingliang_22@163.com>
2014-04-07 23:19:41 +0800

    t

--- com/yangml/learn_day03/pickleTest.py
+++ com/yangml/learn_day03/pickleTest.py
@@ -4,6 +4,7 @@ Created on 2014年4月7日
 
 @author: bling
 '''
+'''
 #1) 将内存中的对象转换成为文本流：
 import pickle
 # define class
@@ -12,7 +13,6 @@ class Bird(object):
     way_of_reproduction = 'egg'
 summer = Bird() # construct an object
 pickelstring = pickle.dumps(object) # serialize object
-
 '''
 import pickle
 # define class
@@ -24,4 +24,3 @@ summer       = Bird()                        # construct an object
 fn           = 'a.pkl'
 with open(fn, 'w') as f:                     # open file with write-mode
     picklestring = pickle.dump(summer, f)   # serialize and save object
-    '''
